46cb6b7aa65586ff55b40295a4e1c8f6822255d4
nicoleta <71027700+nicoleta23-bot@users.noreply.github.com>
2020-09-16 19:37:05 +0300

    Add files via upload

---
+++ buzz1.py
@@ -0,0 +1,15 @@
+print("CV")
+print("Nume Prenume: Vatamaniuc Nicoleta")
+print("Adresa: strada centrala, satul Mindrestii Noi, Raionul Sangerei, Rep. Moldova")
+print("Telefon: 060264063")
+print("E-mail: nicoletavatamaniuc3@gmail.com")
+print("Data, anul nasterii: 23.09.2004")
+
+print("Experienta profesionala: --")
+
+print("Studii:")
+print("     ")
+print(" L.T.,,Alecu Russo'', s. Singereii Noi 2011-2018")
+print(" I.P. L.T.,,Mihai Eminescu'', or Balti 2018- prezent ")
+
+print("Abilitati: sociabila, optimista, responsabila")
